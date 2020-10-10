a07f43188fc09e5b38d06bb4ce396b04f5ef891c
Mauricio Garavaglia <mauri@users.noreply.github.com>
2020-10-10 15:33:21 -0300

    Added first kernel

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# a26
-Atari 2600 Programming Resources
+# Atari 2600 Programming Resources
+
+A collection of examples, and resources for Atari 2600 programming.
+
+
+## Docs
+- [Stella Programmers Guide](https://cdn.hackaday.io/files/1646277043401568/stella.pdf)
+- [2600 Cookbook](https://alienbill.com/2600/cookbook/)
+- [5206 Reference](http://www.obelisk.me.uk/6502/reference.html)
+
+
---
+++ kernel.asm
@@ -0,0 +1,71 @@
+; Simple kernel that renders a rainbow 
+
+; Based on the stella programming series with some adjustments to
+; correct VBLANK handling
+;
+    processor 6502
+    include "vcs.h"
+    include "macro.h"
+
+    SEG
+    ORG $F000       ; start of cartridge memory area
+
+Reset
+    ; clear all memory and registers
+
+    lda #0
+    ldx #0
+Clear
+    sta 0,x
+    inx
+    bne Clear
+
+StartOfFrame
+
+    lda #2
+    sta VSYNC ; set D1 to 1 in VSYNC for 3 scanlines
+
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+
+    lda #0
+    sta VSYNC
+
+    ldx #0
+VerticalBlank
+    sta WSYNC
+    inx
+    cpx #37                 ; compare x with 37 sets zero flag if x equal 37
+    bne VerticalBlank       ; branch to VerticalBlank if zero is clear 
+
+    lda #0
+    sta VBLANK ; end VBLANK
+
+; start picture 
+    ldx #0
+Picture
+    stx COLUBK              ; set background color to x
+
+    sta WSYNC               ; wait for scanline to finish
+    inx                     ; increment x
+    cpx #192                ; compare and sets zero flag if x == 192
+    bne Picture             ; jump if not zero
+; end picture
+
+    lda #%01000010
+    sta VBLANK  ; start VBLANK period
+
+    ; 30 scanlines for overscan
+    REPEAT 30
+        sta WSYNC
+    REPEND
+
+    jmp StartOfFrame
+
+    ORG $FFFA
+
+    .word Reset     ; NMI
+    .word Reset     ; RESET
+    .word Reset     ; IRQ
+
